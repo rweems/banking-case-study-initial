fd64bfeedb971e8548851dfe366ed11a8b47142a
training <a>
2020-02-21 09:44:55 -0500

    initial commit

--- autoloan/src/main/java/com/example/banking/AutoLoanApplication.java
+++ autoloan/src/main/java/com/example/banking/AutoLoanApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AutoLoanApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AutoLoanApplication.class, args);
+        SpringApplication.run(AutoLoanApplication.class,args);
     }
 
 }
